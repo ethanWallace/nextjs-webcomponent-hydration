316fed5c9c4134b83a88474ab02343548d1079ec
Raphael Mayer <raphaelmayer@gmx.de>
2023-01-28 17:09:31 +0100

    update hello component

--- webcomponents/hello.js
+++ webcomponents/hello.js
@@ -2,17 +2,12 @@ export default class HelloComponent extends HTMLElement {
 	#internals = null;
 	constructor() {
 		super();		
+
 		this.#internals = this.attachInternals();
-		console.log(this.#internals);
     const shadow = this.#internals.shadowRoot;
-    this.hours = shadow.querySelector('#hour');
-    this.minutes = shadow.querySelector('#min');
-    this.seconds = shadow.querySelector('#sec'); 
 
 		const button = shadow.firstElementChild;
 		button.addEventListener('click', console.log);
-
-		console.log(shadow);
 	}	
 
 	connectedCallback() {
